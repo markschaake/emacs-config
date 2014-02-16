7409a82cb6d133b69acd9f731c835ec3f6ee2f4a
Mark Schaake <mark.schaake@gmail.com>
2014-02-16 09:59:18 -0800

    Change key bindings to more natural C-x w (more like C-x o)

--- local/windows.el
+++ local/windows.el
@@ -1,6 +1,6 @@
 ;; All custom window-related commands have key prefix C-c w
 
-(global-set-key (kbd "C-c w p") 'windmove-up)
-(global-set-key (kbd "C-c w n") 'windmove-down)
-(global-set-key (kbd "C-c w f") 'windmove-right)
-(global-set-key (kbd "C-c w b") 'windmove-left)
+(global-set-key (kbd "C-x w p") 'windmove-up)
+(global-set-key (kbd "C-x w n") 'windmove-down)
+(global-set-key (kbd "C-x w f") 'windmove-right)
+(global-set-key (kbd "C-x w b") 'windmove-left)
