7d5534c837750f9a5afe4211e33a77e84ff15fed
Mark Schaake <mark.schaake@gmail.com>
2014-02-16 10:13:18 -0800

    Fix error in key binding

--- local/windows.el
+++ local/windows.el
@@ -1,6 +1,6 @@
 ;; All custom window-related commands have key prefix C-c w
 
-(global-set-key (kbd "C-c bo p") 'windmove-up)
+(global-set-key (kbd "C-c o p") 'windmove-up)
 (global-set-key (kbd "C-c o n") 'windmove-down)
 (global-set-key (kbd "C-c o f") 'windmove-right)
 (global-set-key (kbd "C-c o b") 'windmove-left)
